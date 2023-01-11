b5de831e87615aa63c1c4eff89d500a4d448c1e1
did250 <gmldnjs2500@naver.com>
2023-01-11 15:36:06 +0900

    ✨ Refrigerator

--- ssg-ssag/src/main/java/com/shinsegae/ssgssag/member/controller/RefgController.java
+++ ssg-ssag/src/main/java/com/shinsegae/ssgssag/member/controller/RefgController.java
@@ -1,5 +1,58 @@
 package com.shinsegae.ssgssag.member.controller;
 
-public class RefgController {
+import java.io.PrintWriter;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.shinsegae.ssgssag.member.mapper.RefgMapper;
+import com.shinsegae.ssgssag.member.service.RefgService;
+import com.shinsegae.ssgssag.vo.RefgVO;
+
+@Controller
+public class RefgController {
+	
+	@Autowired
+	RefgService service;
+	@Autowired
+	RefgMapper mapper;
+	
+	@GetMapping(value="refg.ssg")
+	public String getList(HttpServletRequest req) {
+		List<RefgVO> list = service.getList("1");
+		for ( int i = 0; i<list.size(); i++) {
+			String name = service.getName(list.get(i).getIng_id());
+			list.get(i).setIng_name(name);
+		}
+		req.setAttribute("list", list);
+		return "member/refg";
+	}
+	
+	@GetMapping(value="delete.ssg")
+	public void delete(@RequestParam("refg_id") String param, HttpServletResponse res) throws Exception {
+		System.out.println("askdjs;adjasldjlsa;j");
+		System.out.println(param);
+		boolean result = service.delete(param);
+		PrintWriter out = res.getWriter();
+		res.setContentType("text/html;charset=utf-8");
+		if (result) {
+			out.print("<script>");
+			out.print("alert('정상적으로 삭제되었습니다.');");
+			out.print("location.href='refg.ssg';");
+			out.print("</script>");
+		}
+		else {
+			out.print("<script>");
+			out.print("alert('삭제 실패');");
+			out.print("location.href='refg.ssg';");
+			out.print("</script>");
+		}
+	}
+	
 }
--- ssg-ssag/src/main/java/com/shinsegae/ssgssag/member/service/RefgService.java
+++ ssg-ssag/src/main/java/com/shinsegae/ssgssag/member/service/RefgService.java
@@ -1,5 +1,49 @@
 package com.shinsegae.ssgssag.member.service;
 
-public class RefgService {
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shinsegae.ssgssag.member.mapper.RefgMapper;
+import com.shinsegae.ssgssag.vo.RefgVO;
+
+
+@Service
+public class RefgService {
+	
+	@Autowired
+	RefgMapper mapper;
+	
+	public List<RefgVO> getList(String ing_id) {
+		List<RefgVO> list = mapper.getList(ing_id);
+		for ( int i = 0; i<list.size(); i++) {
+			Date cur_time = new Date();
+			long remain_mil = list.get(i).getExpire_date().getTime()-cur_time.getTime();
+			long remain_hour = (remain_mil)/3600000%24;
+			long remain_day = remain_mil/3600000/24;
+			if (remain_mil < 0) {
+				list.get(i).setOver(true);
+			}
+			list.get(i).setRemain_day( (int) remain_day );
+			list.get(i).setRemain_hour( (int) remain_hour );
+		}
+		return list;
+	}
+	
+	public String getName(int ing_id) {
+		return mapper.getName(ing_id);
+	}
+	
+	public boolean delete(String id) {
+		int r = mapper.delete(Integer.parseInt(id));
+		if ( r== 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
 }

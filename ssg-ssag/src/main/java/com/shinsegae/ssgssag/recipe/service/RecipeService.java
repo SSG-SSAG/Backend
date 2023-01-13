9e00967145640e05cd6a634d68395db50e2dda03
hyunspace <at.hyunspace@gmail.com>
2023-01-13 15:06:16 +0900

    🐛 문법 오류 수정
    - 자바 문법 오류 수정

--- ssg-ssag/src/main/java/com/shinsegae/ssgssag/recipe/service/RecipeService.java
+++ ssg-ssag/src/main/java/com/shinsegae/ssgssag/recipe/service/RecipeService.java
@@ -30,7 +30,7 @@ public class RecipeService {
 		System.out.println("### obj ### " + obj.get(0));
 		return obj;
 	}
-}	
+
 	public List<RecipeVO> rcp_page(RecipeVO vo) {
 		List<RecipeVO> obj = mapper.rcp_page(vo);
 		
